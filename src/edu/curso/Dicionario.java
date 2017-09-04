d3c6bb85f752bfd9147add4244de822f07af5929
FelipeSantosSouza <felipesnt13@gmail.com>
2017-09-04 11:19:44 -0300

    Exercicio Dicionario

---
+++ src/edu/curso/Dicionario.java
@@ -0,0 +1,34 @@
+package edu.curso;
+
+import java.util.HashMap;
+
+import javax.swing.JOptionPane;
+
+public class Dicionario {
+
+	public static void main(String[] args) {
+
+		HashMap<String, String> dic = new HashMap();
+		
+		String p=JOptionPane.showInputDialog("Digite a palavra: ");
+		try{
+		while (p.compareTo("s")!=0){
+			
+		if(dic.containsKey(p)){
+			System.out.println("Definição :"+dic.get(p));
+		}
+		else{
+			String d = JOptionPane.showInputDialog("Palavra não existe digite a definição: ");;
+			dic.put(p, d);
+		}
+		p=JOptionPane.showInputDialog("Digite a palavra: ");
+		}
+		}
+		catch(NullPointerException e){
+		System.out.println("Saiu");
+		}
+		}
+	
+	}
+
+
